bc08b4cec551980e4db516d143a85515a5a6bc62
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-02-15 02:49:02 +0900

    Create 2.py

---
+++ programmers/Sort/2.py
@@ -0,0 +1,13 @@
+from functools import cmp_to_key
+
+def compare(x,y):
+    if int(x+y)>=int(y+x):
+        return 0
+    else:
+        return -1
+
+def solution(num):
+    str_list = [str(n) for n in num]
+    answer=sorted(str_list, key=cmp_to_key(compare),reverse=True)
+    answer=int(''.join(answer))
+    return str(answer)
