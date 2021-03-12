6648980dca1ba81e23520fea214af291c33e0e52
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-03-13 01:28:12 +0900

    Create 10216.py

---
+++ baekjoon/union-find/10216.py
@@ -0,0 +1,24 @@
+import math
+T=int(input())
+for t in range(T):
+
+    n=int(input())
+    arr=[]
+    for i in range(n):
+        arr.append(list(map(int,input().split(' '))))
+    network=[i for i in range(n)]
+    
+    def isMeet(a,b):
+        return math.sqrt((arr[a][0]-arr[b][0])**2+(arr[a][1]-arr[b][1])**2)<=(arr[a][2]+arr[b][2])
+    
+    for i in range(n):
+        for j in range(i,n):
+            if isMeet(i,j):
+                king=min(network[i],network[j])
+                network[i]=king
+                network[j]=king
+                for k in range(n):
+                    if i==k or k==j:
+                        network[k]=king
+    
+    print(len(set(network)))
